2cbe12ed6c96464521c1f2f2ca086355a49a2471
Gal Amit <gal0688@gmail.com>
2023-05-29 10:47:59 +0300

    t

--- src/Nav.js
+++ src/Nav.js
@@ -1,23 +1,23 @@
 import React from 'react'
 import { Link , Outlet} from 'react-router-dom'
-// import "./Nav.css"
+import "./Nav.css"
 
 export default function Nav() {
   return (    
-      <div className='d-flex'>
-        <div className='container-fluid'>
-        <nav className="navbar navbar-expand-lg navbar-light bg-light ">
-        <ul className='navbar-nav mr-auto'>
-              <li className='nav-item active'> <Link className="nav-link" to='/'>דף הבית</Link></li>
-              <li className='nav-item active'> <Link className="nav-link" to='/about'>אודות</Link></li>
-              <li className='nav-item active'> <Link className="nav-link" to='/login'>התחברות</Link></li>
-              <li className='nav-item active'> <Link className="nav-link" to='/myrecipe'>המתכונים שלי</Link></li>
-              <li className='nav-item active'> <Link className="nav-link" to='/contacus'>צור קשר</Link></li>
-              <li className='nav-item active'> <Link className="nav-link" to='/start'>!התחל</Link></li>
-        </ul>    
-        </nav>
+      <div className=''>
+        <div className='container'>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <ul className='navbar-nav mr-auto'>
+              <li> <Link className="nav-link" to='/'>דף הבית</Link></li>
+              <li> <Link className="nav-link" to='/about'>אודות</Link></li>
+              <li> <Link className="nav-link" to='/login'>התחברות</Link></li>
+              <li> <Link className="nav-link" to='/myrecipe'>המתכונים שלי</Link></li>
+              <li> <Link className="nav-link" to='/contacus'>צור קשר</Link></li>
+              <li> <Link className="nav-link" to='/start'>!התחל</Link></li>
+            </ul>    
+          </nav>
         <Outlet/>
         </div>
-        </div>
+      </div>
   )
 }
